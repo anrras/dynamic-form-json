8a2d7257c728e19b93a1c87024b0ec70ec14de73
Andrés Quintero Arias <aquintero@dataifx.com>
2021-12-13 21:56:22 -0500

    Add ngkmaskModule

--- src/app/custom-form/custom-form.module.ts
+++ src/app/custom-form/custom-form.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { CustomFormRoutingModule } from './custom-form-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 import { JsonFormComponent } from './components/json-form/json-form.component';
 import { InputTextComponent } from './components/input-text/input-text.component';
 import { InputPasswordComponent } from './components/input-password/input-password.component';
+import { ScrollToInvalidFieldDirective } from './directives/scroll-to-invalid-field.directive';
 
 @NgModule({
-  declarations: [JsonFormComponent, InputTextComponent, InputPasswordComponent],
-  imports: [CommonModule, CustomFormRoutingModule, ReactiveFormsModule],
+  declarations: [
+    JsonFormComponent,
+    InputTextComponent,
+    InputPasswordComponent,
+    ScrollToInvalidFieldDirective,
+  ],
+  imports: [
+    CommonModule,
+    CustomFormRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forChild(),
+  ],
   exports: [JsonFormComponent],
 })
 export class CustomFormModule {}
